54eeceaa6b44db85af613bc285dd6ffd5836af40
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-29 15:31:52 +0800

    完善之

--- two_runtime_less_fb/Examples/Clock-iOS/ViewController.m
+++ two_runtime_less_fb/Examples/Clock-iOS/ViewController.m
@@ -12,11 +12,15 @@
 #import "ClockView.h"
 
 
+#import <FBKVOController/FBKVOController.h>
+
+
 @implementation ViewController
 {
   NSMutableArray *_clockViews;
+  FBKVOController *_KVOController;
+  UILabel * _label;
 }
-
 
 - (BOOL)prefersStatusBarHidden
 {
@@ -65,6 +69,44 @@
     [self _addClockView: YES];
     [self _addClockView: NO];
   
+    
+    //
+    
+    //
+    
+    //
+    CGRect f = [[UIScreen mainScreen] bounds];
+    CGFloat width = 250;
+    _label = [[UILabel alloc] initWithFrame: CGRectMake((f.size.width - width)/2 , 100, width, width/2)];
+    _label.textColor = UIColor.blueColor;
+    _label.textAlignment = NSTextAlignmentCenter;
+    _label.font = [UIFont systemFontOfSize: 16];
+    _label.backgroundColor = UIColor.whiteColor;
+    [self.view addSubview: _label];
+    
+    //
+    
+    
+    //
+    
+    //
+    
+    _KVOController = [FBKVOController controllerWithObserver:self];
+    
+    // handle clock change, including initial value
+    [_KVOController observe:[Clock clock] keyPath:@"date" options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew block:^(id  _Nullable observer, id  _Nonnull object, NSDictionary<NSKeyValueChangeKey,id> * _Nonnull change) {
+        
+        // update observer with new value
+        NSDate * date = change[NSKeyValueChangeNewKey];
+        NSDateFormatter *formatter = [NSDateFormatter new];
+        formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss zzz";
+        self->_label.text = [formatter stringFromDate: date];
+    }];
+    
+    
+    
+    
+    
 }
 
 @end
