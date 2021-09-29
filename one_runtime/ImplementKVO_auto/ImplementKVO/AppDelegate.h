5c6017a75d3811d33a8f5b2589abd409367e4eba
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-29 11:02:04 +0800

    add auto

---
+++ one_runtime/ImplementKVO_auto/ImplementKVO/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ImplementKVO
+//
+//  Created by Jz D on 2021/9/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
